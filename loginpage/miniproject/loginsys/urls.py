cc010365ddf77d2bc4d921064906656fe224308c
Merwin <merwinrebello02@gmail.com>
2023-02-04 17:22:04 +0530

    added registration page but not working yet

--- loginpage/miniproject/loginsys/urls.py
+++ loginpage/miniproject/loginsys/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns=[
     path('',views.login,name='login'),
     path('login',views.login,name="login"),
+    path('register',views.register,name='register'),
+    path('login',views.login,name='login'),
 ]
--- loginpage/miniproject/loginsys/views.py
+++ loginpage/miniproject/loginsys/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User,auth
+from django.contrib import messages
+from .models import feats
 
 # Create your views here.
+def index(request):
+    list=feats.objects.all()
+    key=1
+    return render(request,"login.html",{'feature':list,'key':key})
+
 def login(request):
     if request.method=='POST':
         username=request.POST['username']
@@ -11,5 +18,29 @@ def login(request):
             auth.login(request,user)
             return render(request,'login.html',{'text':'sucessfully logged in'})
         else:
-            return render(request,'login.html',{'text':'bad input'})
+            return render(request,'login.html',{'text':' SORRRY!! cannot login'})
+    return render(request,'login.html')
+
+def register(request):
+    if request.method=='POST':
+        list=feats.objects.all()
+        username=request.POST['username']
+        email=request.POST['email']
+        password=request.POST['password']
+        password2=request.POST['password2']
+        if (password==password2):
+            if User.objects.filter(username=username).exists():
+                messages.info(request,'Username Already Used')
+                return redirect('index')        
+            elif User.objects.filter(email=email).exists():
+                messages.info(request,'Email Already Used')
+                return redirect('index')
+            else:
+                user=User.objects.create_user(username=username,email=email,password=password)  
+                key=0 
+                user.save();
+            return render(request,'login.html',{'feature':list,"key":key,'key2':1})
+        else:
+            messages.info(request,'Password Not Same')
+            return redirect('index')
     return render(request,'login.html')
