84040cd97107ac7b32b95bca1593c3883e769242
Merwin <merwinrebello02@gmail.com>
2023-02-25 22:51:03 +0530

    created products table but not  linked with shoppage

--- loginpage/miniproject/loginsys/admin.py
+++ loginpage/miniproject/loginsys/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import products
 # Register your models here.
-admin.site.register(products)
+
+class AdminProduct(admin.ModelAdmin):
+    list_display =['user_name','product_id','amount']
+    
+admin.site.register(products,AdminProduct)
--- loginpage/miniproject/loginsys/models.py
+++ loginpage/miniproject/loginsys/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
+
+
+
 # Create your models here.
 class products(models.Model):
-    product_id = models.IntegerField()
     user_name= models.CharField(max_length=15,default=None)
+    product_id = models.IntegerField()
+    amount= models.IntegerField(default=0)
