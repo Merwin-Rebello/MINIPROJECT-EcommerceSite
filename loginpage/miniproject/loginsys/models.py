5b05a321842e5624e348556aa70032a93e29f7f9
Merwin <merwinrebello02@gmail.com>
2023-02-25 13:21:12 +0530

    added a product table in backend

--- loginpage/miniproject/loginsys/models.py
+++ loginpage/miniproject/loginsys/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class products(models.Model):
+    product_id = models.IntegerField()
+    user_name= models.CharField(max_length=15,default=None)
