9335c39b8656b66f4d8e17f979fba83bdecf9f26
Merwin <merwinrebello02@gmail.com>
2023-02-26 21:20:21 +0530

    added product images

--- loginpage/miniproject/loginsys/admin.py
+++ loginpage/miniproject/loginsys/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Products
 # Register your models here.
 
-class AdminProduct(admin.ModelAdmin):
-    list_display=['prd_name','product_id','price','image']
+from .models import product
+
+admin.site.register(product)
--- loginpage/miniproject/loginsys/models.py
+++ loginpage/miniproject/loginsys/models.py
@@ -1,10 +1,6 @@
 from django.db import models
-# Create your models here.
 
-class Products(models.Model):
-    prd_name=models.CharField(max_length=100,default=None)
-    product_id= models.IntegerField()
+class product(models.Model):
+    name= models.CharField(max_length=100,default=0)
     price= models.IntegerField(default=0)
-    image=models.ImageField(upload_to='productimage/')
-
-
+    image= models.ImageField(upload_to='productimages/')
